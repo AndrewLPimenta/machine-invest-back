3bbaf90471e05d7d76fe8c739bca9a016d0ed6d6
AndrewLPimenta <pimenta.andreews@gmailcom>
2025-08-13 03:36:43 -0300

    criacao de usuario, definicao de perfil de investidor do usuario e classificacao prontas para testes

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  name: z.string().min(1, "Nome é obrigatório"),
-  email: z.string().min(1,"Email inválido").min(1, "Email é obrigatório"),
-  password: z.string().min(6, "Senha Deve ter pelo menos 6 caracteres"),
+  nome: z.string().min(1, "Nome é obrigatório"),
+  email: z.string().email("Email inválido").min(1, "Email é obrigatório"),
+  senha: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
+  confirmarSenha: z.string().min(6, "Senha deve ter pelo menos 6 caracteres")
 });
+
 
 export const loginSchema = z.object({
   email: z.string().min(1,"Email inválido").min(1, "Email é obrigatório"),
