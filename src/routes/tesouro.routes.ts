1972d11edd4c1dc983ac9df8638bacb4d7b85580
AndrewLPimenta <pimenta.andreews@gmailcom>
2025-08-18 15:29:38 -0300

    adicionando api de investimentos conservadores [1]

--- src/routes/tesouro.routes.ts
+++ src/routes/tesouro.routes.ts
@@ -1,9 +1,9 @@
-
 import { Router } from 'express';
 import { fetchTesouro } from '../controllers/tesouro.controller';
 
 const router = Router();
 
+// Rota GET para buscar títulos conservadores
 router.get('/tesouro', fetchTesouro);
 
 export default router;
